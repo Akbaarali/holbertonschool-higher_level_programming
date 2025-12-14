5b7fddc52d6fe82bd148883b1f3bd4327d82d666
Akbaarali <12716@holbertonstudents.com>
2025-12-14 10:34:21 +0000

    3. String representation

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -40,14 +40,11 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
-
-    def __str__(self):
-        if self.__width == 0 or self.__height == 0:
-            return ""
-        lines = []
-        for _ in range(self.__height):
-            lines.append("#" * self.__width)
-        return "\n".join(lines)
-
-    def __repr__(self):
-        return "Rectangle({}, {})".format(self.__width, self.__height)
+	def __str__(self):
+    """Return the string representation of the rectangle with #."""
+    	if self.__width == 0 or self.__height == 0:
+        	return ""
+    	lines = []
+    	for _ in range(self.__height):
+        	lines.append("#" * self.__width)
+   	 return "\n".join(lines)
