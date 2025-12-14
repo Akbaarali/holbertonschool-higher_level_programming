5364bf5a210f7a60837a80a99f61aa7a322f5c8b
Akbaarali <12716@holbertonstudents.com>
2025-12-14 10:59:56 +0000

    3. String representation

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -52,5 +52,9 @@ class Rectangle:
         """Return the string representation of the rectangle with #."""
         if self.__width == 0 or self.__height == 0:
             return ""
-        return "\n".join(["#" * self.__width for _ in range(self.__height)])
+        return "\n".join("#" * self.__width for _ in range(self.__height))
+
+    def __repr__(self):
+        """Return a string representation to recreate a new instance."""
+        return "Rectangle({}, {})".format(self.__width, self.__height)
 
