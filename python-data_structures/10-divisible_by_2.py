820cfd21ee6eaf9b8772bba099690d1b44886146
Akbaarali <12716@holbertonstudents.com>
2025-12-24 11:52:09 +0000

    Add task 10: divisible by 2

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+    """Return a list of True/False depending on divisibility by 2"""
     result = []
-    for num in my_list:
-        if num % 2 == 0:
-            result.append(num)
+
+    for n in my_list:
+        result.append(n % 2 == 0)
+
     return result
