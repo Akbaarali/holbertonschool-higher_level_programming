d42d759b11d0528f8a3f675b4cedf16ad61b5f2b
Akbaarali <12716@holbertonstudents.com>
2025-12-25 16:48:09 +0000

    Fix PEP8 for task 8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Defines a function that returns the dictionary description of an object."""
+"""Return the dictionary description of an object."""
 
 
 def class_to_json(obj):
-    """Return the dictionary description of an object for JSON serialization."""
+    """Return the dictionary description for JSON serialization."""
     return obj.__dict__
