f48949a8f72607bc215e803be1e48266413b1fa5
Akbaarali <12716@holbertonstudents.comProfile image12716@holbertonstudents.com>
2025-12-27 08:57:47 +0000

    Fix Task 8: pycodestyle line length

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
-        print("{}".format(chr(ord(c) - 32) if 97 <= ord(c) <= 122 else c), end="")
+        n = ord(c)
+        print("{}".format(chr(n - 32) if 97 <= n <= 122 else c), end="")
     print()
